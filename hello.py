d8ab9eceb9c342ac03599c9e0567902ead68e5e4
LTarnch <ossiriwanc@gmail.com>
2026-05-15 18:45:17 +0700

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,13 +1,33 @@
-print('Hello World')
-text = '''
-        🌸🌸🌸
-     🌸🌸🌸🌸🌸
-   🌸🌸🌸🌸🌸🌸🌸
-      \  | |  /
-       \ | | /
-        \| |/
-         \_/
-        /___\
+# 🌸 Pretty Sakura Tree 🌸
 
-'''
-print (text)
+import time
+import os
+
+tree = r"""
+                🌸
+              🌸🌸🌸
+            🌸🌸🌸🌸🌸
+          🌸🌸🌸🌸🌸🌸🌸
+        🌸🌸🌸🌸🌸🌸🌸🌸🌸
+      🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸🌸
+
+               ||||
+             \\||||//
+              \\||//
+               \\//
+                \/
+             ___||___
+           /          \
+          /____________\
+
+        ✨ Sakura Tree ✨
+"""
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("\n")
+for line in tree.splitlines():
+    print(line)
+    time.sleep(0.08)
+
+print("\n🌸 Hello World 🌸")
